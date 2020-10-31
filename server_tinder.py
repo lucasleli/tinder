8dde7a76ef10339e2ec5fd8b9bc416cd77043c72
unknown <caiociaron240@gmail.com>
2020-10-31 11:47:35 -0300

    Teste 1 feito

--- server_tinder.py
+++ server_tinder.py
@@ -3,7 +3,7 @@ from flask import Flask, jsonify, request
 app = Flask(__name__)
 
 pessoas = []
-
+interesses = {}
 
 @app.route('/pessoas', methods=['GET'])
 def lista():
@@ -13,6 +13,7 @@ def lista():
 @app.route('/pessoas', methods=['POST'])
 def adiciona():
     pessoas.append(request.json)
+    interesses[request.json['id']] = []
     return jsonify({'status':'ok'})
 
 
@@ -26,14 +27,26 @@ def busca_id(id_pessoa):
 
 @app.route('/reseta', methods=['POST'])
 def reset():
-    global pessoas
+    global pessoas, interesses
     pessoas = []
+    interesses = {}
     return jsonify(pessoas)
 
 
-@app.route('/sinalizar_interesse/<int:pessoa1>/<int:pessoa2>', methods=['PUT'])
+@app.route('/interesses/<int:pessoa>', methods=['GET'])
+def listainteresses(pessoa):
+    if pessoa in interesses:
+        return jsonify(interesses[pessoa])
+    return jsonify({'erro': 'pessoa não encontrada'}), 404
+
+
+@app.route('/sinalizar_interesse/<int:pessoa1>/<int:pessoa2>/', methods=['PUT'])
 def sinalizar(pessoa1, pessoa2):
-    pass
+    if (pessoa1 in interesses) and (pessoa2 in interesses):
+        interesses[pessoa1].append(pessoa2)
+        return jsonify(interesses)
+    return jsonify({'erro': 'uma das pessoas não existe'}), 404
+
 
 if __name__ == '__main__':
     app.run(host = 'localhost', port = 5003, debug = True)
