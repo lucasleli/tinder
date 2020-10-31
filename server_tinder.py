706f40ce756be1884fc5f7579279b6a5a715138f
unknown <caiociaron240@gmail.com>
2020-10-31 13:59:01 -0300

    AC Completa

--- server_tinder.py
+++ server_tinder.py
@@ -4,6 +4,8 @@ app = Flask(__name__)
 
 pessoas = []
 interesses = {}
+matches = {}
+
 
 @app.route('/pessoas', methods=['GET'])
 def lista():
@@ -14,7 +16,8 @@ def lista():
 def adiciona():
     pessoas.append(request.json)
     interesses[request.json['id']] = []
-    return jsonify({'status':'ok'})
+    matches[request.json['id']] = []
+    return jsonify({'status': 'ok'})
 
 
 @app.route('/pessoas/<int:id_pessoa>', methods=['GET'])
@@ -27,9 +30,10 @@ def busca_id(id_pessoa):
 
 @app.route('/reseta', methods=['POST'])
 def reset():
-    global pessoas, interesses
+    global pessoas, interesses, matches
     pessoas = []
     interesses = {}
+    matches = {}
     return jsonify(pessoas)
 
 
@@ -40,13 +44,58 @@ def listainteresses(pessoa):
     return jsonify({'erro': 'pessoa não encontrada'}), 404
 
 
-@app.route('/sinalizar_interesse/<int:pessoa1>/<int:pessoa2>/', methods=['PUT'])
+@app.route('/sinalizar_interesse/<int:pessoa1>/<int:pessoa2>/',
+           methods=['PUT'])
 def sinalizar(pessoa1, pessoa2):
     if (pessoa1 in interesses) and (pessoa2 in interesses):
-        interesses[pessoa1].append(pessoa2)
+
+        for pessoa in pessoas:
+            if pessoa['id'] == pessoa1:
+                pessoa1 = pessoa
+            if pessoa['id'] == pessoa2:
+                pessoa2 = pessoa
+
+        try:
+            if pessoa2['sexo'] in pessoa1['buscando']:
+                interesses[pessoa1['id']].append(pessoa2['id'])
+            else:
+                return jsonify({'erro': 'interesse incompativel'}), 400
+        except KeyError:
+            interesses[pessoa1['id']].append(pessoa2['id'])
+
+        if (
+           (pessoa1['id'] in interesses[pessoa2['id']]) and (
+               (pessoa2['id'] in interesses[pessoa1['id']]))
+           ):
+            matches[pessoa1['id']].append(pessoa2['id'])
+            matches[pessoa2['id']].append(pessoa1['id'])
+
+        return jsonify(interesses)
+    return jsonify({'erro': 'uma pessoa não existe'}), 404
+
+
+@app.route('/sinalizar_interesse/<int:pessoa1>/<int:pessoa2>/',
+           methods=['DELETE'])
+def retirarinteresse(pessoa1, pessoa2):
+    if (pessoa1 in interesses) and (pessoa2 in interesses):
+        match1 = pessoa1 in interesses[pessoa2]
+        match2 = pessoa2 in interesses[pessoa1]
+        interesses[pessoa1].remove(pessoa2)
+
+        if (match1 and match2):
+            matches[pessoa1].remove(pessoa2)
+            matches[pessoa2].remove(pessoa1)
+
         return jsonify(interesses)
     return jsonify({'erro': 'uma das pessoas não existe'}), 404
 
 
+@app.route('/matches/<int:pessoa>', methods=['GET'])
+def retmatches(pessoa):
+    if pessoa in matches:
+        return jsonify(matches[pessoa])
+    return jsonify({'erro': 'uma das pessoas não existe'}), 404
+
+
 if __name__ == '__main__':
-    app.run(host = 'localhost', port = 5003, debug = True)
+    app.run(host='localhost', port=5003, debug=True)
